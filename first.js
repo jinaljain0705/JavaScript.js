23e72c2496974afe4923d89cffbe5c3b5d91756c
Jinal Jain <jinaljain624@gmail.com>
2025-06-04 16:01:58 +0530

    commit

---
+++ first.js
@@ -0,0 +1,9 @@
+let fullName = "tony stark";
+let age = 24;
+let price = 99.99;
+let radius = 14;
+let a = null;
+let y = undefined;
+let isFollow = false;
+let isUnfollow = true; 
+console.log(age);  
