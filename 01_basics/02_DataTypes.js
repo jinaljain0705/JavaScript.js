b22d64c1209d42d4fa5f561de595fc456fe8edd5
Jinal Jain <jinaljain624@gmail.com>
2025-06-05 17:06:09 +0530

    commit

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,28 @@
+"use strict";  // treat all JS code as nwewr version
+
+// alert("3 + 3") // we are using nodejs, not browser
+console.log(3 + 3); // code readability should be high
+
+console.log("Heyy Jinny"); 
+ 
+
+// Data Types
+// String
+let name = "Jinny";
+let age = 26;
+let isLoggedIn = true;
+let state;
+
+// number => 2 to power 53
+// bigint"
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// objects
+console.log(typeof name); // string
+console.log(typeof age); // number
+console.log(typeof null); // object (this is a bug in JS)
+console.log(typeof undefined); // boolean
