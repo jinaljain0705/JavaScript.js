27af63e661a23d30d29b9aea2674592eb65e28b7
Jinal Jain <jinaljain624@gmail.com>
2025-06-05 15:19:21 +0530

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 1234567890;
+let accountEmail = "jinny@google.com"
+var accountPassword = "12345"
+accountCity = "Silvassa"
+let accouuntState;
+
+
+// accountId = 2
+
+console.log(accountId); 
+
+accountEmail = "hc@hc.com"
+accountPassword = "12212212"
+accountCity = "Mumbai"
+
+console.table([accountId, accountEmail, accountEmail, accountCity, accouuntState]);
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
