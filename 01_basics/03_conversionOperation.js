6975f10c6b4dcbbbb2662535b1463e804957b554
Jinal Jain <jinaljain624@gmail.com>
2025-06-06 13:52:28 +0530

    commit

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+// let score = 33;
+// let score = Null;
+let score = undifined;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; faulse => 0
+
+let isLoggedIn = "Jinny";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true 0 => false
+// "" => false
+// "Jinny" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
