af7ba9204dfb4acab0df114a19e23c8ed4e032d5
Jinal Jain <jinaljain624@gmail.com>
2025-06-25 15:00:24 +0530

    commit

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0); // avoid this type of conversion
+console.log(null == 0); // avoid this type of conversion
+console.log(null >= 0); // avoid this type of conversion
+
+console.log(undefined == 0); // avoid this type of conversion
+
+// === means strict check
+
+console.log("2" === 2);
