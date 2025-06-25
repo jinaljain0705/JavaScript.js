a4d8eba852fb934d9779cd9b3d21edac2fc498ec
Jinal Jain <jinaljain624@gmail.com>
2025-06-25 15:22:18 +0530

    commit

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+//  # Primitive DataTypes
+
+//  7 types : String, Number, Boolearn, Null, 
+//             Undefined, Symbol, BigInt.
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+// const id = symbol('123')
+// const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+// const bigNumber = 345432567686543n
+
+//  Reference (Non primitive)
+
+//  Array, Objects, Functions
+
+const heros =["Ironman", "Thor", "Hulk"]
+let myObj = {
+    Name : "jinny",
+    age : 26,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
+
+//https://262.ecma-international.org/5.1/#sec-11.4.3
+ 
